a94b872258e2811398abacf7b2d22d3b804c7047
Prashansa0108 <39775093+Prashansa0108@users.noreply.github.com>
2021-08-16 12:35:26 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "ca-central-1"
-  access_key = "AKIA36JD3WNOJJQ6BQEM"
-  secret_key = "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"
+  access_key = "XXXXXXXXXXX"
+  secret_key = "XXXXXXXXXXXXXXXXXX"
 }
